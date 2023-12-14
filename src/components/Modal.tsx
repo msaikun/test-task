6226c20bdccfb520c44c4a152a2cd2b3bad8c397
msaikun <marynasaikun@gmail.com>
2023-12-14 23:13:47 +0200

    Added modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle
+}                    from '@mui/material';
+
+interface IModalProps {
+  open         : boolean;
+  content      : ReactNode;
+  submitText?  : string;
+  cancelText?  : string;
+  title?       : string;
+  handleClose  : () => void;
+  handleSubmit : () => void;
+}
+
+export const Modal = ({
+  submitText = 'Submit',
+  cancelText = 'Cancel',
+  content,
+  title,
+  open,
+  handleClose,
+  handleSubmit
+}: IModalProps) => {
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      {title && <DialogTitle>{title}</DialogTitle>}
+      
+      <DialogContent>{content}</DialogContent>
+
+      <DialogActions>
+        <Button onClick={handleClose}>{cancelText}</Button>
+        <Button onClick={handleSubmit}>{submitText}</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
