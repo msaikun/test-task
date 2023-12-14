d5703eecf5b9225f3996206d371f39e90e51802f
msaikun <marynasaikun@gmail.com>
2023-12-14 20:11:16 +0200

    First commit for tree view component

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import ReactDOM          from 'react-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+import { TreeViewComponent } from './components/TreeView/TreeView';
+import { GlobalStyles, theme } from './utils/theme';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <TreeViewComponent />
+  </ThemeProvider>,
+  document.getElementById('root')
 );
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
