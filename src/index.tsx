8363df155b287fbbd577a26be26f570b36b33a5c
msaikun <marynasaikun@gmail.com>
2023-12-15 03:17:50 +0200

    Push to gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,49 +4,6 @@ import ReactDOM          from 'react-dom';
 import { GlobalStyles, theme } from './utils/theme';
 import App                     from './App';
 
-const data: any = {
-  id: 1,
-  name: "Cars",
-  children: [
-    {
-      id: 2,
-      name: "Sport Cars",
-      children: [
-        {
-          id: 24,
-          name: "Porsche"
-        },
-        {
-          id: 25,
-          name: "Ferrari"
-        },
-        {
-          id: 26,
-          name: "McLaren"
-        }
-      ]
-    },
-    {
-      id: 3,
-      name: "Classic Cars",
-      children: [
-        {
-          id: 34,
-          name: "1957 Corvette"
-        },
-        {
-          id: 35,
-          name: "Volkswagen Beetle"
-        },
-        {
-          id: 36,
-          name: "Bentley"
-        }
-      ]
-    }
-  ]
-};
-
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
