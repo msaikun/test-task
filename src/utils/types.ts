0ac2ced4d13d6301d607926311c857aff33fac59
msaikun <marynasaikun@gmail.com>
2024-01-09 08:43:12 +0200

    First commit for google map

--- src/utils/types.ts
+++
@@ -1,8 +0,0 @@
-export type Truthy<T> = T extends false | '' | 0 | null | undefined ? never : T;
-
-export type NestedAccordionNode = {
-  children?   : NestedAccordionNode[];
-  id          : string;
-  isDisabled? : boolean;
-  label       : string;
-};
